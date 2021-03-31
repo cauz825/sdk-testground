9c8354aa1dde354d4754442309486116b7b8e35b
MCauz <michaelhcausey@gmail.com>
2021-03-31 15:28:49 -0700

    testing

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+const mtg = require('mtgsdk')
+
 function Search() {
+
+    function handleSearch(e) {
+        e.preventDefault();
+        console.log('test')
+    }
+
     return(
-        <div>
-            Search Component
-        </div>
+        <form>
+            <input type='text' name='name' placeholder='Card Name' />
+            <button onSubmit={handleSearch} type='submit'>Search</button>
+        </form>
     )
 }
 
