168a70892d4cc7c8d1d8bbaacde04de51b74b7f9
MCauz <michaelhcausey@gmail.com>
2021-04-09 10:53:17 -0700

    collection component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Search from './components/Search'
+import Collection from './components/Collection'
 
 function App() {
   return(
